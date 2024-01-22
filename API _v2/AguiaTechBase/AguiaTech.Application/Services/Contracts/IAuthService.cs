975640239947f9f569f34c636db16e54af47d8f6
Pablo Venino <pablo.venino@mult-e.com.br>
2024-01-21 21:19:02 -0300

    feat: Estrutura API e Application

---
+++ API _v2/AguiaTechBase/AguiaTech.Application/Services/Contracts/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AguiaTech.Application.Services.Contracts;
+public interface IAuthService
+{
+    Task<BaseServiceResult> Login();
+    Task<BaseServiceResult> RefreshToken();
+}
